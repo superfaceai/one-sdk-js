1629087df331a0ef205c8d842539128a541ed953
Lukas Valenta <lukas.valenta@applifting.cz>
2020-05-19 11:09:15 +0200

    feat: Project initialization

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  endOfLine: "lf",
+}
