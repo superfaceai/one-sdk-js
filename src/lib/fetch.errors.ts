48bec55b55092601ce6671a3e1b15ac1c772c523
Martin Albert <martin.albert.187@gmail.com>
2021-07-13 14:30:01 +0200

    fix: add getter for compatibility with event adapter

--- src/lib/fetch.errors.ts
+++ src/lib/fetch.errors.ts
@@ -1,24 +1,41 @@
 import { ErrorBase } from '../internal/errors';
 
-export interface NetworkError {
+interface NetworkError {
   kind: 'network';
   issue: 'unsigned-ssl' | 'dns' | 'timeout' | 'reject';
 }
 
-export interface RequestError {
+interface RequestError {
   kind: 'request';
   issue: 'abort' | 'timeout';
 }
 
-export class NetworkFetchError extends ErrorBase {
-  constructor(public issue: NetworkError['issue']) {
-    super('NetworkError', `Fetch failed because of ${issue} issue`);
+export class FetchError extends ErrorBase {
+  constructor(
+    public override kind: string,
+    public issue: NetworkError['issue'] | RequestError['issue']
+  ) {
+    super(kind, `Fetch failed because of ${issue} issue`);
   }
 }
 
-export class RequestFetchError extends ErrorBase {
-  constructor(public issue: RequestError['issue']) {
-    super('RequestError', `Fetch failed because of ${issue} issue`);
+export class NetworkFetchError extends FetchError {
+  constructor(public override issue: NetworkError['issue']) {
+    super('NetworkError', issue);
+  }
+
+  get normalized(): NetworkError {
+    return { kind: 'network', issue: this.issue };
+  }
+}
+
+export class RequestFetchError extends FetchError {
+  constructor(public override issue: RequestError['issue']) {
+    super('RequestError', issue);
+  }
+
+  get normalized(): RequestError {
+    return { kind: 'request', issue: this.issue };
   }
 }
 
