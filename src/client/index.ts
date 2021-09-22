06e315b30195e52461bac54d40a829036c1c9440
Martin Albert <martin.albert.187@gmail.com>
2021-09-22 12:13:27 +0200

    feat: export PerformError from client module

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,6 +1,6 @@
 export { SuperfaceClient, createTypedClient, typeHelper } from './client';
 export { Profile, ProfileConfiguration, TypedProfile } from './profile';
 export { Provider, ProviderConfiguration } from './provider';
-export { UseCase } from './usecase';
+export { PerformError, UseCase } from './usecase';
 export * from './query';
 export * from './profile-provider';
