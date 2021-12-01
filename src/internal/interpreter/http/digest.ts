25f00cdd00a341b53e0a13d9ebacef47576ea5fa
Jakub Vacek <jakub.vacek@dxheroes.io>
2021-12-01 09:10:10 +0100

    feat: add sha256 to digest auth

--- src/internal/interpreter/http/digest.ts
+++ src/internal/interpreter/http/digest.ts
@@ -4,10 +4,14 @@ import { UnexpectedError } from '../..';
 import { FetchInstance } from './interfaces';
 
 /**
+ * Represents algorithm used in Digest auth.
+ */
+type DigestAlgorithm = 'MD5' | 'MD5-sess' | 'SHA-256' | 'SHA-256-sess';
+/**
  * Represents values extracted from initial digest call
  */
 type DigestAuthValues = {
-  algorithm: 'MD5' | 'MD5-sess';
+  algorithm: DigestAlgorithm;
   scheme: string;
   realm: string;
   qop: 'auth' | 'auth-int' | undefined;
@@ -64,20 +68,22 @@ export class DigestHelper {
   ): string {
     const uri = new URL(url).pathname;
 
-    //Default H1 for MD5 algorithm
-    let ha1 = DigestHelper.computeMD5Hash(
+    //Default H1
+    let ha1 = DigestHelper.computeHash(
+      digest.algorithm,
       `${this.user}:${digest.realm}:${this.password}`
     );
 
-    //MD5-sess H1 contains original H1 and also nonce and cnonce
-    if (digest.algorithm === 'MD5-sess') {
-      ha1 = DigestHelper.computeMD5Hash(
+    //sess H1 contains original H1 and also nonce and cnonce
+    if (digest.algorithm.endsWith('-sess')) {
+      ha1 = DigestHelper.computeHash(
+        digest.algorithm,
         `${ha1}:${digest.nonce}:${digest.cnonce}`
       );
     }
 
-    //H2 is same for MD5 and M5-sess
-    const ha2 = DigestHelper.computeMD5Hash(`${method}:${uri}`);
+    //H2 is same for default and sess
+    const ha2 = DigestHelper.computeHash(digest.algorithm, `${method}:${uri}`);
 
     const ncString = `00000000${this.nc}`.slice(-8);
 
@@ -91,7 +97,7 @@ export class DigestHelper {
     }
 
     //Hash response
-    const hashedResponse = DigestHelper.computeMD5Hash(response);
+    const hashedResponse = DigestHelper.computeHash(digest.algorithm, response);
 
     //Build final auth header
     const opaqueString = digest.opaque ? `opaque="${digest.opaque}",` : '';
@@ -113,37 +119,51 @@ export class DigestHelper {
     return {
       scheme: header.split(/\s/)[0],
       algorithm: this.extractAlgorithm(header),
-      realm: (this.parse(header, 'realm', false) || '').replace(/["]/g, ''),
-      opaque: this.parse(header, 'opaque'),
-      qop: this.parseQop(header),
-      nonce: this.parse(header, 'nonce') || '',
+      realm: (this.extract(header, 'realm', false) || '').replace(/["]/g, ''),
+      opaque: this.extract(header, 'opaque'),
+      qop: this.extractQop(header),
+      nonce: this.extract(header, 'nonce') || '',
       cnonce: this.makeNonce(),
     };
   }
 
   /**
-   * Extracts "MD5" or "MD5-sess" algorithm from passed header. "MD5" is default value.
-   * @param header string containing algorithm type
-   * @returns "MD5" or "MD5-sess"
+   * Extracts "MD5", "MD5-sess", "SHA-256" or "SHA-256-sess" algorithm from passed header. "MD5" is default value.
+   * @param rawHeader string containing algorithm type
+   * @returns "MD5", "MD5-sess", "SHA-256" or "SHA-256-sess"
    */
-  private extractAlgorithm(header: string): 'MD5' | 'MD5-sess' {
-    const parsedHeader = this.parse(header, 'algorithm');
+  private extractAlgorithm(rawHeader: string): DigestAlgorithm {
+    const extractedValue = this.extract(rawHeader, 'algorithm');
 
-    if (parsedHeader !== undefined && parsedHeader.includes('MD5-sess')) {
-      return 'MD5-sess';
+    if (extractedValue !== undefined) {
+      if (extractedValue.includes('MD5')) {
+        return 'MD5';
+      } else if (extractedValue.includes('MD5-sess')) {
+        return 'MD5-sess';
+      } else if (extractedValue.includes('SHA-256')) {
+        return 'SHA-256';
+      } else if (extractedValue.includes('SHA-256-sess')) {
+        return 'SHA-256-sess';
+      } else {
+        //Throw when we get unexpected value
+        throw new UnexpectedError(
+          `Digest auth failed, parameter "algorithm" has unexpected value`,
+          extractedValue
+        );
+      }
     }
 
-    // when not specified
+    //When not specified use MD5
     return 'MD5';
   }
 
-  private parseQop(rawAuth: string): 'auth' | 'auth-int' | undefined {
+  private extractQop(rawHeader: string): 'auth' | 'auth-int' | undefined {
     // Following https://en.wikipedia.org/wiki/Digest_access_authentication
     // to parse valid qop
     // Samples
     // : qop="auth,auth-init",realm=
     // : qop=auth,realm=
-    const parsedQop = this.parse(rawAuth, 'qop');
+    const parsedQop = this.extract(rawHeader, 'qop');
 
     if (parsedQop !== undefined) {
       const qops = parsedQop.split(',');
@@ -172,7 +192,7 @@ export class DigestHelper {
     return uid;
   }
 
-  private parse(raw: string, field: string, trim = true): string | undefined {
+  private extract(raw: string, field: string, trim = true): string | undefined {
     //TODO: field should be case-insensitive
     const regex = new RegExp(`${field}=("[^"]*"|[^,]*)`, 'i');
     const match = regex.exec(raw);
@@ -181,7 +201,25 @@ export class DigestHelper {
     return undefined;
   }
 
-  private static computeMD5Hash(data: string): string {
-    return createHash('MD5').update(data).digest('hex');
+  /**
+   * Computes hash from data using specified algorithm
+   * @param algorithm used to compute hash
+   * @param data data to be hashed
+   * @returns hashed data
+   */
+  private static computeHash(algorithm: DigestAlgorithm, data: string): string {
+    let usedAlgorithm;
+    if (algorithm.startsWith('MD5')) {
+      usedAlgorithm = 'MD5';
+    } else if (algorithm.startsWith('SHA-256')) {
+      usedAlgorithm = 'sha256';
+    } else {
+      throw new UnexpectedError(
+        `Digest auth failed, parameter "algorithm" has unexpected value`,
+        algorithm
+      );
+    }
+    
+return createHash(usedAlgorithm).update(data).digest('hex');
   }
 }
