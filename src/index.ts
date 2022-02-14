1d8f1dfe8491d9d3507375271d81e4485c3c744f
Martin Albert <martin.albert.187@gmail.com>
2022-02-14 10:16:32 +0100

    chore: remove export of private module

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ export * from './client';
 export * from './interfaces';
 export * from './lib';
 export * from './internal';
-export * from './private';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
 export const VERSION: string = packageJson.version;
