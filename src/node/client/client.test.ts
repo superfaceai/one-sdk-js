2a06d37a9f52b62523dc8a0158144ac7a73a1aea
Jakub Vacek <vacjak@seznam.cz>
2022-07-18 11:13:26 +0200

    chore: clean up tests

--- src/node/client/client.test.ts
+++ src/node/client/client.test.ts
@@ -1,14 +1,6 @@
-import { EXTENSIONS } from '@superfaceai/ast';
 import { mocked } from 'ts-jest/utils';
 
-import {
-  NotFoundError,
-  profileFileNotFoundError,
-  sourceFileExtensionFoundError,
-  unsupportedFileExtensionError,
-} from '../../core';
-import { fetchProfileAst } from '../../core/registry';
-import { err, ok } from '../../lib';
+import { ProfileConfiguration, resolveProfileAst } from '../../core';
 import { MockClient, mockProfileDocumentNode } from '../../mock';
 import { getProviderForProfile, SuperJson } from '../../schema-tools';
 
@@ -40,308 +32,52 @@ const mockSuperJson = new SuperJson({
   },
 });
 
-const mockSuperJsonCustomPath = new SuperJson({
-  profiles: {
-    test: '2.1.0',
-  },
-  providers: {
-    quz: {},
-  },
-});
-
 afterEach(() => {
   jest.useRealTimers();
   jest.resetAllMocks();
 });
 
 jest.mock('../../core/registry');
+jest.mock('../../core/profile/resolve-profile-ast');
 jest.mock('../../core/events/failure/event-adapter');
 
 describe('superface client', () => {
   describe('getProfile', () => {
-    it('rejects when profile does not exists', async () => {
+    it('retruns Profile instance', async () => {
+      const ast = mockProfileDocumentNode({
+        name: 'testy/mctestface',
+        version: {
+          major: 1,
+          minor: 0,
+          patch: 0,
+        },
+      });
+      mocked(resolveProfileAst).mockResolvedValue(ast);
       const client = new MockClient(mockSuperJson);
 
-      await expect(client.getProfile('does/not-exist')).rejects.toThrow(
-        'Profile "does/not-exist" not found in super.json'
+      const profile = await client.getProfile('testy/mctestface');
+
+      expect(profile.ast).toEqual(ast);
+      expect(profile.configuration).toEqual(
+        new ProfileConfiguration('testy/mctestface', '1.0.0')
       );
-    });
-
-    describe('when using entry with version only', () => {
-      it('returns a valid profile when profile is found in grid', async () => {
-        const client = new MockClient(mockSuperJson, {
-          fileSystemOverride: {
-            path: {
-              resolve: (...pathSegments: string[]) => pathSegments.join(''),
-            },
-            readFile: jest.fn(path => {
-              expect(path).toMatch('testy/mctestface@0.1.0.supr.ast.json');
-
-              return Promise.resolve(
-                ok(
-                  JSON.stringify(
-                    mockProfileDocumentNode({
-                      name: 'mctestface',
-                      scope: 'testy',
-                      version: {
-                        major: 0,
-                        minor: 1,
-                        patch: 0,
-                      },
-                    })
-                  )
-                )
-              );
-            }),
-          },
-        });
-
-        const profile = await client.getProfile('testy/mctestface');
-        expect(profile.configuration.version).toBe('0.1.0');
-      });
-
-      it('returns a valid profile when profile is found in registry', async () => {
-        mocked(fetchProfileAst).mockResolvedValue(
-          mockProfileDocumentNode({
-            name: 'mctestface',
-            scope: 'testy',
-            version: {
-              major: 0,
-              minor: 1,
-              patch: 0,
-            },
-          })
-        );
-        const client = new MockClient(mockSuperJson, {
-          fileSystemOverride: {
-            path: {
-              resolve: (...pathSegments: string[]) => pathSegments.join(''),
-            },
-            readFile: jest.fn(path => {
-              expect(path).toMatch('testy/mctestface@0.1.0.supr.ast.json');
-
-              return Promise.resolve(err(new NotFoundError('test')));
-            }),
-          },
-        });
-
-        const profile = await client.getProfile('testy/mctestface');
-        expect(profile.configuration.version).toBe('0.1.0');
-        expect(fetchProfileAst).toHaveBeenCalledWith(
-          'testy/mctestface@0.1.0',
-          client.config,
-          client.crypto,
-          expect.anything(),
-          expect.anything()
-        );
-      });
-    });
-
-    describe('when using entry with filepath only', () => {
-      it('rejects when profile points to a non-existent path', async () => {
-        const mockError = profileFileNotFoundError(
-          '../foo.supr.ast.json',
-          'foo'
-        );
-        const client = new MockClient(mockSuperJson, {
-          fileSystemOverride: {
-            readFile: () => Promise.resolve(err(mockError)),
-          },
-        });
-
-        await expect(client.getProfile('foo')).rejects.toThrow(mockError);
-      });
-
-      it('rejects when profile points to a path with .supr extension', async () => {
-        const client = new MockClient(mockSuperJson);
-
-        await expect(client.getProfile('evil/foo')).rejects.toThrow(
-          sourceFileExtensionFoundError(EXTENSIONS.profile.source)
-        );
-      });
-
-      it('rejects when profile points to a path with unsupported extension', async () => {
-        const client = new MockClient(mockSuperJson);
-
-        await expect(client.getProfile('bad/foo')).rejects.toThrow(
-          unsupportedFileExtensionError(
-            mockSuperJson.resolvePath('../foo.ts'),
-            EXTENSIONS.profile.build
-          )
-        );
-      });
-
-      it('returns a valid profile when it points to existing path', async () => {
-        const client = new MockClient(mockSuperJson, {
-          fileSystemOverride: {
-            readFile: jest.fn(path => {
-              expect(path).toMatch('foo.supr.ast.json');
-
-              return Promise.resolve(
-                ok(
-                  JSON.stringify(
-                    mockProfileDocumentNode({
-                      name: 'foo',
-                      version: {
-                        major: 1,
-                        minor: 0,
-                        patch: 1,
-                        label: 'test',
-                      },
-                    })
-                  )
-                )
-              );
-            }),
-          },
-        });
-
-        const profile = await client.getProfile('foo');
-        expect(profile.configuration.version).toBe('1.0.1-test');
-      });
-
-      it('rejects when loaded file is not valid ProfileDocumentNode', async () => {
-        const invalidAst: any = mockProfileDocumentNode({ name: 'foo' });
-        invalidAst.kind = 'broken';
-        const client = new MockClient(mockSuperJson, {
-          fileSystemOverride: {
-            readFile: () => Promise.resolve(ok(JSON.stringify(invalidAst))),
-          },
-        });
-
-        await expect(client.getProfile('foo')).rejects.toThrow();
-      });
-    });
-
-    describe('when using version property', () => {
-      it('returns a valid profile when profile is found in grid', async () => {
-        const client = new MockClient(mockSuperJson, {
-          fileSystemOverride: {
-            path: {
-              resolve: (...pathSegments: string[]) => pathSegments.join(''),
-            },
-            readFile: jest.fn(path => {
-              expect(path).toMatch('baz@1.2.3.supr.ast.json');
-
-              return Promise.resolve(
-                ok(
-                  JSON.stringify(
-                    mockProfileDocumentNode({
-                      name: 'baz',
-                      version: {
-                        major: 1,
-                        minor: 2,
-                        patch: 3,
-                      },
-                    })
-                  )
-                )
-              );
-            }),
-          },
-        });
-
-        const profile = await client.getProfile('baz');
-        expect(profile.configuration.version).toBe('1.2.3');
-      });
-
-      it('returns a valid profile when profile is found in registry', async () => {
-        mocked(fetchProfileAst).mockResolvedValue(
-          mockProfileDocumentNode({
-            name: 'baz',
-            version: {
-              major: 1,
-              minor: 2,
-              patch: 3,
-            },
-          })
-        );
-        const client = new MockClient(mockSuperJson, {
-          fileSystemOverride: {
-            path: {
-              resolve: (...pathSegments: string[]) => pathSegments.join(''),
-            },
-            readFile: jest.fn(path => {
-              expect(path).toMatch('baz@1.2.3.supr.ast.json');
-
-              return Promise.resolve(err(new NotFoundError('test')));
-            }),
-          },
-        });
-
-        const profile = await client.getProfile('baz');
-        expect(profile.configuration.version).toBe('1.2.3');
-        expect(fetchProfileAst).toHaveBeenCalledWith(
-          'baz@1.2.3',
-          client.config,
-          client.crypto,
-          expect.anything(),
-          expect.anything()
-        );
-      });
-    });
-
-    describe('when using file property', () => {
-      it('rejects when profile points to a non-existent path', async () => {
-        const mockError = profileFileNotFoundError(
-          '../bar.supr.ast.json',
-          'bar'
-        );
-        const client = new MockClient(mockSuperJson, {
-          fileSystemOverride: {
-            readFile: () => Promise.resolve(err(mockError)),
-          },
-        });
-
-        await expect(client.getProfile('bar')).rejects.toThrow(mockError);
-      });
-
-      it('returns a valid profile when it points to existing path', async () => {
-        const client = new MockClient(mockSuperJson, {
-          fileSystemOverride: {
-            readFile: jest.fn(path => {
-              expect(path).toMatch('bar.supr.ast.json');
-
-              return Promise.resolve(
-                ok(
-                  JSON.stringify(
-                    mockProfileDocumentNode({
-                      name: 'bar',
-                      version: {
-                        major: 1,
-                        minor: 0,
-                        patch: 1,
-                      },
-                    })
-                  )
-                )
-              );
-            }),
-          },
-        });
-
-        const profile = await client.getProfile('bar');
-        expect(profile.configuration.version).toBe('1.0.1');
-      });
-
-      it('rejects when loaded file is not valid ProfileDocumentNode', async () => {
-        const invalidAst: any = mockProfileDocumentNode({ name: 'bar' });
-        invalidAst.kind = 'broken';
-        const client = new MockClient(mockSuperJson, {
-          fileSystemOverride: {
-            readFile: () => Promise.resolve(ok(JSON.stringify(invalidAst))),
-          },
-        });
-
-        await expect(client.getProfile('bar')).rejects.toThrow();
-      });
     });
   });
 
   describe('getProviderForProfile', () => {
     it('throws when providers are not configured', async () => {
       expect(() =>
-        getProviderForProfile(mockSuperJsonCustomPath, 'foo')
+        getProviderForProfile(
+          new SuperJson({
+            profiles: {
+              test: '2.1.0',
+            },
+            providers: {
+              quz: {},
+            },
+          }),
+          'foo'
+        )
       ).toThrow(
         'Profile "foo" needs at least one configured provider for automatic provider selection'
       );
