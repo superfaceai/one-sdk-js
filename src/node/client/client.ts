2c9b6808ac8819771e85a77d3d6d5331d8cf9b16
Jakub Vacek <vacjak@seznam.cz>
2022-07-19 08:31:26 +0200

    chore: comment

--- src/node/client/client.ts
+++ src/node/client/client.ts
@@ -125,6 +125,9 @@ export abstract class SuperfaceClientBase {
   constructor(
     options?: {
       superJson?: SuperJson | SuperJsonDocument;
+      /**
+       * Flag that can be used to disable caching to filesystem. `true` by default.
+       */
       cache?: boolean;
     } & Partial<Omit<IConfig, 'cachePath'>>
   ) {
@@ -195,8 +198,7 @@ export abstract class SuperfaceClientBase {
 
 export class SuperfaceClient
   extends SuperfaceClientBase
-  implements ISuperfaceClient
-{
+  implements ISuperfaceClient {
   /** Gets a profile from super.json based on `profileId` in format: `[scope/]name`. */
   public async getProfile(profileId: string): Promise<Profile> {
     return this.internal.getProfile(profileId);
